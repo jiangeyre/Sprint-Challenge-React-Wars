65663d32935ae5b54f381a454edcb8d71026c749
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-11-08 09:22:49 -0800

    styling for the people cards

--- starwars/src/components/PersonCard.js
+++ starwars/src/components/PersonCard.js
@@ -6,14 +6,14 @@ const Card = styled.div`
     color: white;
     height: 450px;
     border: 3px solid #968e83;
-    border-radius: 5px;
+    border-radius: 10px;
     box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22), inset 0 15px 20px rgba(0, 0, 0, 0.22);
     overflow: hidden;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    opacity: 0.7;
+    opacity: 0.8;
 `;
 
 const PersonCard = (props) => {
@@ -22,11 +22,11 @@ const PersonCard = (props) => {
             <h2>{props.name}</h2>
                 <div>
                     {/* <p>Species: {props.species}</p> */}
-                    <p>Gender: {props.gender}</p>
+                    <p>Gender: {props.gender.charAt(0).toUpperCase() + props.gender.slice(1)}</p>
                     {/* <p>Homeworld: {props.homeworld}</p> */}
                     <p>Year of Birth: {props.birthYear}</p>
-                    <p>Height: {props.height}cm</p>
-                    <p>Weight: {props.mass}kg</p>
+                    <p>Height: {props.height} cm</p>
+                    <p>Weight: {props.mass} kg</p>
                     <p>Hair Color: {props.hairColor}</p>
                     <p>Eye Color: {props.eyeColor}</p>
                     <p>Skin Color: {props.skinColor}</p>
